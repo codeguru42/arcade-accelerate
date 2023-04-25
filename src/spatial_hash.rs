906e12653d4ab87a17e7dbbc11a51619ec40ab46
Code-Apprentice <codeguru42@gmail.com>
2023-04-25 14:56:03 -0600

    Set cell_size

--- src/spatial_hash.rs
+++ src/spatial_hash.rs
@@ -3,13 +3,24 @@ use pyo3::prelude::*;
 #[pyclass]
 pub struct SpatialHash {
     #[pyo3(get, set)]
-    num: i32
+    cell_size: i32
 }
 
 #[pymethods]
 impl SpatialHash {
     #[new]
-    fn new(value: i32) -> Self {
-        SpatialHash { num: value }
+    fn new(cell_size: i32) -> Self {
+        Self { cell_size }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_spatial_hash_new() {
+        let spatial_hash = SpatialHash { cell_size: 42 };
+        assert_eq!(42, spatial_hash.cell_size)
     }
 }
