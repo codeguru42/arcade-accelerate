7abd978113486290a096c2002f69902d3941b2d4
Code-Apprentice <codeguru42@gmail.com>
2023-04-25 15:59:48 -0600

    Implement hash()

--- src/spatial_hash.rs
+++ src/spatial_hash.rs
@@ -3,14 +3,21 @@ use pyo3::prelude::*;
 #[pyclass]
 pub struct SpatialHash {
     #[pyo3(get, set)]
-    cell_size: u32,
+    cell_size: u16,
 }
 
 #[pymethods]
 impl SpatialHash {
     #[new]
-    fn new(cell_size: u32) -> Self {
+    fn new(cell_size: u16) -> Self {
         Self { cell_size }
+    }
+
+    fn hash(&self, point: (i32, i32)) -> (i32, i32) {
+        (
+            point.0 / i32::from(self.cell_size),
+            point.1 / i32::from(self.cell_size),
+        )
     }
 }
 
@@ -22,5 +29,14 @@ mod tests {
     fn test_new() {
         let spatial_hash = SpatialHash { cell_size: 42 };
         assert_eq!(42, spatial_hash.cell_size)
+    }
+
+    #[test]
+    fn test_hash() {
+        let spatial_hash = SpatialHash { cell_size: 10 };
+        let point = (20, 30);
+        let result = spatial_hash.hash(point);
+        let expected = (2, 3);
+        assert_eq!(expected, result);
     }
 }
